eaac048f2a2f23220d502dab4d76560032e949e0
icgw <icgw@outlook.com>
2019-04-10 17:53:20 +0800

    Added dijkstra and data process method into model.

--- CodeCraft-2019/model.cpp
+++ CodeCraft-2019/model.cpp
@@ -5,16 +5,99 @@
  * Distributed under terms of the GPL license.
  */
 
+#include <algorithm> // std::reverse
+#include <queue>     // std::priority_queue
 
-#include <limits>
 #include "model.hpp"
 
-Feedback
-Model::dijkstra(Info2Point &in2p)
+void
+Model::initIndex()
 {
-  std::vector<double> dist(this->graph_.get_size(),
-                           std::numeric_limits<double>::infinity());
-  auto st = in2p.start_time;
+  // XXX: after model initialization.
+  auto sz = this->raw_crosses_.size();
+  this->adjacency_.resize(sz);
 
-  return {};
+  // TODO: create cross_id_to_index, wait to qualify.
+  for (auto i = 0; i < sz; ++i) {
+    this->cross_id_to_index_[this->raw_crosses_[i].id] = i;
+  }
+
+  // TODO: create cross_index_to_road_info_, adjacency_. wait to qualify.
+  //   -- FINISH extract raw_roads information completely.
+  sz = this->raw_roads_.size();
+  int from_idx, to_idx;
+  for (auto i = 0; i < sz; ++i) {
+    from_idx = this->cross_id_to_index_[this->raw_roads_[i].from];
+    to_idx   = this->cross_id_to_index_[this->raw_roads_[i].to];
+    this->adjacency_[from_idx].push_back(to_idx);
+    this->cross_index_to_road_info_[{ from_idx, to_idx }].id      = this->raw_roads_[i].id;
+    this->cross_index_to_road_info_[{ from_idx, to_idx }].len     = this->raw_roads_[i].len;
+    this->cross_index_to_road_info_[{ from_idx, to_idx }].speed   = this->raw_roads_[i].speed;
+    this->cross_index_to_road_info_[{ from_idx, to_idx }].channel = this->raw_roads_[i].channel;
+    if (0 != this->raw_roads_[i].is_duplex) {
+      this->cross_index_to_road_info_[{ to_idx, from_idx }].id      = this->raw_roads_[i].id;
+      this->cross_index_to_road_info_[{ to_idx, from_idx }].len     = this->raw_roads_[i].len;
+      this->cross_index_to_road_info_[{ to_idx, from_idx }].speed   = this->raw_roads_[i].speed;
+      this->cross_index_to_road_info_[{ to_idx, from_idx }].channel = this->raw_roads_[i].channel;
+      this->adjacency_[to_idx].push_back(from_idx);
+    }
+  }
+
+  // TODO: car?
+
+  return;
+}
+
+Feedback
+Model::dijkstra(StartEndInfo &start_end,
+                std::function<bool (const NodeInfo&, const NodeInfo&)> cmp,
+                std::function<int (StartEndInfo&, NodeInfo&)> cost)
+{
+  Feedback fb;
+  std::priority_queue<NodeInfo,
+                      std::vector<NodeInfo>,
+                      decltype(cmp)> pq(cmp);
+
+  std::vector<int> trace (this->size_);
+  // std::map<int, int> dist;
+  std::vector<int> dist (this->size_);
+
+  NodeInfo src;
+  src.cost_time    = start_end.start_time;
+  src.index        = start_end.from_index;
+  src.volumn       = this->node_info_[src.index].volumn;
+  dist[src.index]  = cost(start_end, src);
+  trace[src.index] = -1;
+
+  int len, limit, min_v, v_cost_time;
+  while (!pq.empty()) {
+    NodeInfo u = pq.top();
+    pq.pop();
+    for (auto &v_idx : this->adjacency_[u.index]) {
+      auto v = this->node_info_[v_idx];
+      int w  = cost(start_end, v);
+      if (dist[v_idx] > dist[u.index] + w) {
+        dist[v_idx]  = dist[u.index] + w;
+        len          = this->cross_index_to_road_info_[{ u.index, v_idx }].len;
+        limit        = this->cross_index_to_road_info_[{ u.index, v_idx }].speed;
+        min_v        = std::min(start_end.speed, limit);
+        v_cost_time  = (len + min_v - 1) / min_v;
+        v.cost_time  = v_cost_time + u.cost_time;
+        trace[v_idx] = u.index;
+
+        pq.push(v);
+      }
+    }
+  }
+
+  int to = start_end.to_index;
+  fb.t_path.push_back(to);
+  while (trace[to] != -1) {
+    fb.t_path.push_back(trace[to]);
+  }
+  fb.t_path.push_back(start_end.from_index);
+  reverse(fb.t_path.begin(), fb.t_path.end());
+  fb.cost_time = dist[start_end.to_index];
+
+  return fb;
 }
--- CodeCraft-2019/model.hpp
+++ CodeCraft-2019/model.hpp
@@ -10,10 +10,39 @@
 
 #include <vector>
 #include <map>
+#include <functional> // std::function
 #include <unordered_map>
-#include <utility>
+#include <utility>    // std::pair
 
-/*{{{ RawCar, RawRoad, RawCross. Probably require to modify */
+/*{{{ DEFINE MACRO */
+#define   CAR_ID                  0
+#define   CAR_FROM                1
+#define   CAR_TO                  2
+#define   CAR_SPEED               3
+#define   CAR_PLAN_TIME           4
+#define   CAR_PRIORITY            5
+#define   CAR_PRESET              6
+
+#define   ROAD_ID                 0
+#define   ROAD_LEN                1
+#define   ROAD_SPEED              2
+#define   ROAD_CHANNEL            3
+#define   ROAD_FROM               4
+#define   ROAD_TO                 5
+#define   ROAD_IS_DUPLEX          6
+
+#define   CROSS_ID                0
+#define   CROSS_UP                1
+#define   CROSS_RIGHT             2
+#define   CROSS_DOWN              3
+#define   CROSS_LEFT              4
+
+#define   PRESET_CAR_ID           0
+#define   PRESET_CAR_START_TIME   1
+#define   PRESET_CAR_ROAD_START   2
+/*}}}*/
+
+/*{{{ RawCar, RawRoad, RawCross.*/
 struct RawCar {
   int id, from, to, speed, plan_time, priority, preset;
 };
@@ -28,99 +57,158 @@ struct RawCross {
 /*}}}*/
 
 struct Feedback {
-  int start_time;
-  std::vector<int> pre_path;
-  double cost;
-  int suggest_start_time;
+  // int start_time, sugeest_start_time;
+  std::vector<int> t_path;
+  int cost_time;
 };
 
-struct Info2Point {
-  int start_time;
-  int from;
-  int to;
-  int speed;
+struct StartEndInfo {
+  int start_time, from_index, to_index, speed;
 };
 
-struct InfoRoad {
-  int len;
-  int max_capacity;
-  int limit_speed;
-
-  // time_stamp : current_num_of_cars
-  std::unordered_map<int, int> live_time_window;
+struct NodeInfo {
+  int index, cost_time, volumn;
 };
 
-struct PassBy {
-  int start, end;
-  int leave;
+struct RoadInfo {
+  int id, len, speed, channel;
 };
 
-struct InfoPass {
-  int start_time;
-  std::vector<PassBy> pass_by;
-};
-
-/*{{{ class Graph : store adjacency list */
-class Graph {
-public:
-  Graph(std::size_t sz)
-    : size_(sz) { this->adjacency_.resize(sz); }
-  ~Graph() {}
-  std::size_t get_size() const;
-
-  bool add_edge(const std::size_t u, const std::size_t v, const bool dir);
-
-private:
-  Graph() = default;
-  std::size_t size_;
-  std::vector<std::vector<int>> adjacency_;
-};
-
-inline std::size_t
-Graph::get_size()
-  const
-{
-  return this->size_;
-}
-
-inline bool
-Graph::add_edge(const std::size_t u,
-                const std::size_t v,
-                const bool dir)
-{
-  if (u >= this->size_ || v >= this->size_) {
-    return false;
-  }
-
-  this->adjacency_[u].push_back(v);
-  if (!dir) {
-    this->adjacency_[v].push_back(u);
-  }
-
-  return true;
-}
+/*{{{ @Deprecated. class Graph : store adjacency list */
+/*
+ * class Graph {
+ * public:
+ *   Graph(std::size_t sz)
+ *     : size_(sz) { this->adjacency_.resize(sz); }
+ *   ~Graph() {}
+ *   std::size_t get_size() const;
+ *   std::vector<int> get_adjacency(std::size_t u) const;
+ *   bool add_edge(const std::size_t u, const std::size_t v, const bool dir);
+ * 
+ * private:
+ *   Graph() = default;
+ *   std::size_t size_;
+ *   std::vector<std::vector<int>> adjacency_;
+ * };
+ * 
+ * inline std::size_t
+ * Graph::get_size()
+ *   const
+ * {
+ *   return this->size_;
+ * }
+ * 
+ * inline std::vector<int>
+ * Graph::get_adjacency(std::size_t u)
+ *   const
+ * {
+ *   if (u >= this->size_) return {};
+ *   return this->adjacency_[u];
+ * }
+ * 
+ * inline bool
+ * Graph::add_edge(const std::size_t u,
+ *                 const std::size_t v,
+ *                 const bool dir)
+ * {
+ *   if (u >= this->size_ || v >= this->size_) {
+ *     return false;
+ *   }
+ * 
+ *   this->adjacency_[u].push_back(v);
+ *   if (!dir) {
+ *     this->adjacency_[v].push_back(u);
+ *   }
+ * 
+ *   return true;
+ * }
+ */
 /*}}}*/
 
 class Model {
 public:
-  // Model(input data) ?
+  // Model(std::size_t num_of_crosses) : size_(num_of_crosses) {}
+
+  Model(const std::vector<std::vector<int>> &cars,
+        const std::vector<std::vector<int>> &roads,
+        const std::vector<std::vector<int>> &crosses,
+        const std::vector<std::vector<int>> &preset_cars);
+
   ~Model() {}
-  // TODO: dijkstra
-  Feedback dijkstra(Info2Point &in2p);
-  void update_roads_info(InfoPass &inps); // XXX: update roads_history_
+
+  // TODO: not finish yet.
+  void initIndex();
+
+  // TODO: not in consideration yet.
+  // void update_roads_info(InfoPass &inps);
+
+  // TODO: wait to qualify.
+  Feedback dijkstra(StartEndInfo &start_end,
+                    std::function<bool (const NodeInfo&, const NodeInfo&)> cmp,
+                    std::function<int (StartEndInfo&, NodeInfo&)> cost);
+
+  // XXX: require to design.  rate = start_time ^ 2 / all_car_require_time ?
+  double time_rate(const int start_time, const int all_car_require_time);
+
+  // TODO: wait to qualify.
+  void increase_volumn(std::vector<int> &nodes);
+  // TODO: wait to qulify.
+  std::vector<int> transform_path(std::vector<int> &nodes);
 
 private:
   Model() = default;
+  int size_;
 
   std::vector<RawCar>      raw_cars_;
   std::vector<RawRoad>    raw_roads_;
   std::vector<RawCross> raw_crosses_;
 
-  std::map<std::pair<int, int>, RawRoad*> uv_to_roads_;
+  // XXX: Deprecated.
+  // std::map<std::pair<int, int>, RawRoad*> uv_to_roads_;
+  // std::map<std::pair<int, int>, InfoRoad> roads_;
+  // std::map<std::pair<int, int>, int> cross_index_to_road_id_;
+  // std::map<int, std::vector<NodeInfo>> adjacency_;
+  // std::vector<int> counter_;
 
-  // (c1, c2) --> {0: .., 1: .., ... n: .. } updating after calling schedule.
-  Graph graph_;
-  std::map<std::pair<int, int>, InfoRoad> roads_history_;
+  // TODO: wait to qualify.
+  std::map<int, int> cross_id_to_index_;
+
+  // TODO: wait to qualify.
+  std::map<std::pair<int, int>, RoadInfo> cross_index_to_road_info_;
+
+  // TODO: wait to qualify.
+  std::vector<std::vector<int>> adjacency_;
+
+  // TODO: wait to qualify.
+  std::vector<NodeInfo> node_info_;
 };
+
+inline
+Model::Model(const std::vector<std::vector<int>> &cars,
+             const std::vector<std::vector<int>> &roads,
+             const std::vector<std::vector<int>> &crosses,
+             const std::vector<std::vector<int>> &preset_cars)
+{
+  // TODO: obtain information and transform into raw_***s_.
+}
+
+inline void
+Model::increase_volumn(std::vector<int> &nodes)
+{
+  for (auto n : nodes) {
+    ++(this->node_info_[n].volumn);
+  }
+  return;
+}
+
+inline std::vector<int>
+Model::transform_path(std::vector<int> &nodes) {
+  std::vector<int> road_path;
+  auto sz = nodes.size();
+  for (auto i = 1; i < sz; ++i) {
+    road_path.push_back(this->cross_index_to_road_info_[{ nodes[i - 1], nodes[i] }].id);
+  }
+  return road_path;
+}
 
 #endif // ifndef _MODEL_HPP_
