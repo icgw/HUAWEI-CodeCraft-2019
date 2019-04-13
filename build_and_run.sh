c66cda780dc78cfc872c6acfb9ca98727bc08404
icgw <icgw@outlook.com>
2019-04-13 20:49:03 +0800

    delete some scripts

--- build_and_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sh build.sh
-cd bin
-# ./CodeCraft-2019 ../config/car.txt ../config/road.txt ../config/cross.txt ../config/presetAnswer.txt ../config/answer.txt
-time ./CodeCraft-2019 ../2-map-training-1/car.txt ../2-map-training-1/road.txt ../2-map-training-1/cross.txt ../2-map-training-1/presetAnswer.txt ../2-map-training-1/answer.txt
