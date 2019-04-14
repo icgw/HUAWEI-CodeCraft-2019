363d953228f3c6250ffcdad8906575d3e322c870
icgw <icgw@outlook.com>
2019-04-14 11:16:40 +0800

    Added schedule jude testing data.

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,3 +2,8 @@
 sh build.sh
 cd bin
 ./CodeCraft-2019 ../config/car.txt ../config/road.txt ../config/cross.txt ../config/presetAnswer.txt ../config/answer.txt
+cd ../CodeCraft-2019/judge/
+make
+make clean
+@echo Testing schdule judge...
+./main ../../training-answer/car.txt ../../training-answer/road.txt ../../training-answer/cross.txt ../../training-answer/presetAnswer.txt ../../training-answer/answer.txt
